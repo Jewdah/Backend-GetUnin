12b916eebd6a881b0089e5acd59cbdd45d9ee01c
Jewdah <88810236+Jewdah@users.noreply.github.com>
2023-10-13 17:41:14 -0500

    first commit
    base de proyecto

---
+++ src/test/java/getunin/example/Backend/GetUnin/BackendGetUninApplicationTests.java
@@ -0,0 +1,13 @@
+package getunin.example.Backend.GetUnin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGetUninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
